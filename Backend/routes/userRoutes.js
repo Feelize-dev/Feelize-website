d3df0b0a7f30dfb6318fb65a60145d080faac83a
AleefAhammed <ahammedaleefk@gmail.com>
2025-10-22 13:49:58 +0530

    Update userRoutes.js

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 import express from "express"
-import { verifyUser, verifyUserWithToken } from "../middleware/verifyUser.js";
+import { verifyFirebaseToken } from "../middleware/verifyUser.js";
 import { createNewUser } from "../controller/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/sessionLogin", verifyUserWithToken, createNewUser)
+router.get("/sessionLogin", verifyFirebaseToken, createNewUser)
 
 export default router;
